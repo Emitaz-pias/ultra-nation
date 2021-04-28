26f4936cd1dbc9c9588b23c62919bc480f20252c
Emitaz-pias <61836021+Emitaz-pias@users.noreply.github.com>
2021-04-28 07:40:06 +0600

    bultiple data passed to the cild comoponent

--- src/Country/Country.js
+++ src/Country/Country.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React from "react";
+const style = {  width:"50px" };
 
 const Country = (props) => {
-    return (
-        <div>
-            <h2>{props.country.name}</h2>
-        </div>
-    );
+  const { name, flag, population } = props.country;
+  return (
+    <div>
+      <h2>{name}</h2>
+      <p>{population}</p>
+      <img style={style} src={flag} alt="" />
+    </div>
+  );
 };
 
 export default Country;
